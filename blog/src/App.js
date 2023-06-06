837c557333f3d9a73584d41e7829ee3260cbb50a
vicomarquez <vicomarquezmuniz@gmail.com>
2023-06-06 20:17:22 -0300

    add basic home style

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>El Escarraman Music</h1>
+        <button className='Home-button'>Home</button>
+        <button className='Bandas-button'>Bandas</button>
+        <button className='Países-button'>Países</button>
+        </div>
+      <div className='App-body'>
+        <p>PUBLICIDAD</p>
+      </div>
+      <div className='App-footer'>
+        <p>El Escarraman Music 2023</p>
+      </div>
+    </>
   );
 }
 
